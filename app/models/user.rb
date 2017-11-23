e54949d7bda468bdc0939d2fce32a781797e6f28
Shane Richman <Shane.M.Richman@gmail.com>
2017-11-23 02:02:47 +0000

    fixed password signup bug, and check cart when not signed idin bug. added style sheet for submit form

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 5 }
+  validates :password, presence: true
   validates :password_confirmation, presence: true, length: { minimum: 5 }
 
   def self.authenticate_with_credentials(email, password)
